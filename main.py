d4d66d60cfc5813dbbb09f29292396d9239f6c47
Pankaj Shukla - p0s0cad <luciferhell58@gmail.com>
2025-10-24 09:45:02 +0530

    unfixed

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI
 
-from controller.chat_controller import router as chat_router
 from controller.workflow_controller import router as workflow_router
 
 app = FastAPI()
 
 # Include the workflow controller routes
 app.include_router(workflow_router)
-app.include_router(chat_router)
 
 if __name__ == "__main__":
     import uvicorn
--- workflow/cert_workflow/agents/qna_agent.py
+++ workflow/cert_workflow/agents/qna_agent.py
@@ -14,7 +14,11 @@ from langchain.tools import tool
 class QnAAgentResponse(BaseModel):
     # Response fields for QnA agent
     response: str = Field(
-        description="this field must have the same response which agent gives to the user"
+        description="this field contains a summarized version of agent response"
+    )
+    has_answer:bool = Field(
+        description="this field indicates if the agent was able to come an answer",
+        default=False,
     )
 
 @tool
@@ -25,6 +29,7 @@ def retrieve_context(query:str) -> List[dict]:
     Response you will be getting as List[dict]
     """
     service = RetrieverService()
+    print(f"called retrieve context with query: {query}")
     return service.retrieve_data(query)
 
 prompt = """
@@ -35,16 +40,18 @@ Guidelines:
 - Use simple words and friendly sentences.
 - Encourage curiosity by adding a fun fact or a follow-up question.
 - Never guess or invent information. Only use what your tools provide.
-- If you don't have an answer, say: "At the moment I don't have any answer for this."
-- Always answer in the response format:  
-  response: str (your answer to the child’s question)
+- If you don't have an answer, give 
+    response: "At the moment I don't have any answer for this."
+    has_answer: False
 
 Examples:
 1. Child: "Why do birds sing?"
    Response: "Birds sing to talk to each other and sometimes to find friends. Did you know some birds can copy sounds they hear? What sound would you like a bird to copy?"
+    has_answer: True
 
 2. Child: "How do plants grow?"
    Response: "Plants grow by using sunlight, water, and air. Their leaves catch sunlight to make food. Have you ever tried growing a plant from a seed?"
+    has_answer: True
 
 Do not perform any other actions.
 """
@@ -56,17 +63,25 @@ qna_agent = create_react_agent(
     ],
     prompt=prompt,
     name="qna_agent",
-    response_format=QnAAgentResponse
+    response_format=QnAAgentResponse,
+    debug=True,
 )
 
 def qna_agent_node(state: ChatState) -> dict:
     """"""
     # Get the response from the QnA agent
+    # TODO THIS ISN'T WORKING
     result = qna_agent.invoke({"messages": state.messages})
     result: QnAAgentResponse = result["structured_response"]
-
-    return {
+    if result.has_answer:
+        return {
+            "messages": AIMessage(
+                content=result.response
+            )
+        }
+    else:
+        return {
         "messages": AIMessage(
-            content=result.response
-        )
+        content="As of now i don't have answer , is there any other question i can help"
+         )
     }
