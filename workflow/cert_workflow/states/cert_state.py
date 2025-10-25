abd183f56ccb334e67b8faf6c5eb88a6e686f01d
Pankaj Shukla - p0s0cad <luciferhell58@gmail.com>
2025-10-25 17:43:28 +0530

    added cert state

--- workflow/cert_workflow/states/cert_state.py
+++ workflow/cert_workflow/states/cert_state.py
@@ -10,5 +10,28 @@ class ChatState(BaseModel):
     messages: Annotated[Sequence[BaseMessage], add_messages] = Field(
         title="messages",
         description="The messages exchanged in the conversation",
-        default=[],
+        default=[]
     )
+
+    userName: str = Field(
+        title="name",
+        description="The name of the user",
+    )
+
+    language: str = Field(
+        title="language",
+        description="The language in which user would like to communicate it can be either english or hindi",
+        default="English"
+    )
+
+    voice_response: str = Field(
+        title="voice response",
+        description="The voice response of the ai",
+    )
+
+    html_response: str = Field(
+        title="visual response",
+        description="The html response of the ai",
+    )
+
+
